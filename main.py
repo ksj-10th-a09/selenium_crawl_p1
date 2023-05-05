370c0a0219556e627b5281e80c0fa15113d79c27
iCAROS7 <hominlab@gmail.com>
2023-05-06 08:39:55 +0900

    M: HTML 코드 전체를 크롤링 하도록 수정

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
 
-URL = 'https://www.google.com'
+URL = 'https://minnote.net'
 
 options = webdriver.EdgeOptions()
 options.add_argument('window-size=1920,1080')
@@ -24,18 +25,8 @@ driver.get(url=URL)
 # driver.close()
 
 # Wait until load
-# driver.implicitly_wait(time_to_wait=5) # Implicit
-
-search_box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-
-search_box.send_keys('minnote.net')
-search_box.send_keys(Keys.RETURN)
-
-elements = driver.find_elements_by_xpath('//*[@id="rso"]/div[*]/div/div[1]/a/h3/span')
-
-for element in elements:
-    print(element.text)
-    print(element.text, file=open('output.txt', 'w', encoding='utf-8'))
-
-sleep(3)
-driver.close()
+driver.implicitly_wait(time_to_wait=10) # Implicit
+html_source = driver.page_source
+f=open("output.txt","w", encoding='utf-8')
+f.write(html_source)
+f.close()
