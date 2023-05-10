935f67ce3b97fbe91167f5b1459a5386a55f247f
iCAROS7 <hominlab@gmail.com>
2023-05-10 16:07:11 +0900

    M: 메서드 분리

--- get_html.py
+++ get_html.py
@@ -2,37 +2,36 @@ import time
 from urllib.parse import urlparse
 from selenium import webdriver
 
-URL = 'https://minnote.net'
-domain = urlparse(URL).netloc
+SCROLL_PAUSE_SEC = 1
 
-options = webdriver.EdgeOptions()
-options.add_argument('window-size=1920,1080')
+def init(in_domain, in_browser='Edge'):
+    global options
+    global domain
+    global browser
+    global driver
 
-driver = webdriver.Edge(executable_path='msedgedriver', options=options)
-driver.get(url=URL)
+    domain = in_domain
+    browser = in_browser
 
 # Wait until load
 driver.implicitly_wait(time_to_wait=10) # Implicit
 
 SCROLL_PAUSE_SEC = 1
 
-# Get Scroll Height
-last_height = driver.execute_script("return document.body.scrollHeight")
+        # Get scroll height again after scroll down
+        new_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
-    # Scroll down to end
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(SCROLL_PAUSE_SEC)
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-    # Get scroll height again after scroll down
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+    html_source = driver.page_source
+    try:
+        file_name = in_url.replace('http://', '').replace('https://', '').replace('\n', '').replace('\\', '_').replace(domain, '')
 
-html_source = driver.page_source
-title = driver.title
+        f=open("./" + domain + '/' + file_name + ".txt", "w", encoding='utf-8')
+        f.write(html_source)
+        f.close()
 
-f=open("./" + domain + "/" + title + ".txt", "w", encoding='utf-8')
-f.write(html_source)
-f.close()
+    except Exception as e:
+        print(e)
