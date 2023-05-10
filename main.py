645df8efe325fbaf59aff221c133c26db2a1bc50
iCAROS7 <hominlab@gmail.com>
2023-05-10 16:08:55 +0900

    M: 파라메타 및 쓰기 알고리즘 수정

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import argparse
 import os
+import get_html
 from crawler import Crawler
 from urllib.parse import urlparse
 
+global links
+
 # Parameter parsing
 parser = argparse.ArgumentParser(description="K-Shield Jr. 10th Python Crawling Project")
-parser.add_argument('--url', action='store', default='', help='분석을 원하는 사이트 주소를 입력해 주십시오.')
-parser.add_argument('--exclude', action='store', default='', help='URL에서 제외할 단어 혹은 패턴을 입력해 주십시오.')
+parser.add_argument('-u', '--url', action='store', default='', help='분석을 원하는 사이트 주소를 입력해 주십시오.', required=True)
+parser.add_argument('-e', '--exclude', action='store', default='', help='URL에서 제외할 단어 혹은 패턴을 입력해 주십시오.')
+parser.add_argument('-b', '--browser', action='store', default='Edge', help='사용할 브라우저를 선택 합니다')
 
 # Init var
 args = parser.parse_args()
@@ -14,12 +18,21 @@ url = args.url.rstrip("/")
 domain = urlparse(url).netloc
 
 # Make dir with domain name
-os.makedirs('./' + domain, exist_ok=True)
+path = './' + domain
+if not os.path.exists(path):
+	os.makedirs('./' + domain, exist_ok=True)
+	crawler = Crawler(url, exclude=args.exclude, no_verbose=True)
+	print('Please Wait for scan what target site...\nStart for ' + url)
+	links = crawler.start()
 
-crawler = Crawler(url, exclude=args.exclude, no_verbose=False)
-links = crawler.start()
+	# Write all url to text file
+	with open('./' + domain + '/_sitemap_.txt', "w") as file:
+		for link in links:
+			file.write("{0}\n".format(link).lstrip('/'))
 
-# Write all url to text file
-with open('./' + domain + '/sitemap.txt', "w") as file:
-	for link in links:
-		file.write("{0}{1}\n".format(url, link))
+f = open(path + '/_sitemap_.txt', 'r')
+links = f.readlines()
+
+get_html.init(domain)
+for link in links:
+	get_html.start(url + '/' + link)
