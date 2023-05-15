8f9297d5600fe441279c632ebdb5a6c149d3204b
iCAROS7 <hominlab@gmail.com>
2023-05-16 00:48:02 +0900

    A: FileNotFoundError 예외 핸들링

--- main.py
+++ main.py
@@ -29,9 +29,15 @@ if not os.path.exists(path):
 		for link in links:
 			file.write("{0}\n".format(link).lstrip('/'))
 
-f = open(path + '/_sitemap_.txt', 'r')
-links = f.readlines()
+try:
+	f = open(path + '/_sitemap_.txt', 'r')
 
-get_html.init(domain)
-for link in links:
-	get_html.start(url + '/' + link)
+except FileNotFoundError:
+	print('ERROR: File Not Found')
+	
+else:
+	links = f.readlines()
+
+	get_html.init(domain)
+	for link in links:
+		get_html.start(url + '/' + link)
