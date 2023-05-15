649c344e26f7a671ff3e729e91036ae479f12436
iCAROS7 <hominlab@gmail.com>
2023-05-16 00:47:27 +0900

    M: 불필요 출력 제거

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ path = './' + domain
 if not os.path.exists(path):
 	os.makedirs('./' + domain, exist_ok=True)
 	crawler = Crawler(url, exclude=args.exclude, no_verbose=True)
-	print('Please Wait for scan what target site...\nStart for ' + url)
 	links = crawler.start()
 
 	# Write all url to text file
