f84f03d2da5d52d1f926a066e3e51c7a593b6ae1
iCAROS7 <hominlab@gmail.com>
2023-05-17 10:01:45 +0900

    A: Tag 검색 기능 추가 및 path.exists 조건문 개선

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(description="K-Shield Jr. 10th Python Crawling 
 parser.add_argument('-u', '--url', action='store', default='', help='분석을 원하는 사이트 주소를 입력해 주십시오.', required=True)
 parser.add_argument('-e', '--exclude', action='store', default='', help='URL에서 제외할 단어 혹은 패턴을 입력해 주십시오.')
 parser.add_argument('-b', '--browser', action='store', default='Edge', help='사용할 브라우저를 선택 합니다')
+parser.add_argument('-t', '--tag', action='store', default='True', help='Tag를 검색합니다')
 
 # Init var
 args = parser.parse_args()
@@ -19,11 +20,9 @@ url = args.url.rstrip("/")
 domain = urlparse(url).netloc
 
 # Make dir with domain name
-path = './' + domain
-if not os.path.exists(path):
-	os.makedirs('./' + domain, exist_ok=True)
-	crawler = Crawler(url, exclude=args.exclude, no_verbose=False)
-	links = crawler.start()
+path = f'./{domain}'
+if not os.path.exists(path) or not os.path.exists(path + '/_sitemap_.txt'):
+    os.makedirs('./' + domain, exist_ok=True)
 
     crawler = Crawler(url, exclude=args.exclude, no_verbose=False)
     links = crawler.start()
